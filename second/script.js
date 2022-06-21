5223cfa3b57e6cf66a99d2bf11b63a5fad0fa2e5
Nikita Churakov <motiki839@gmail.com>
2022-06-21 16:14:40 +0300

    init

---
+++ second/script.js
@@ -0,0 +1,17 @@
+      // const initialState = [
+        //     {name: 'nick', tel: '85325325', id:'1234'}
+        // ]
+        // const Store = new Proxy(
+        // {
+        //     state: initialState
+        // }, 
+        // {
+        //     get(target, prop) {
+        //         // console.log(prop, target)
+        //         return target
+        //     },
+        //     set(target, prop, value) {
+        //         console.log(target, prop, value)
+        //     }
+        // })
+        // console.log(Store.state)
